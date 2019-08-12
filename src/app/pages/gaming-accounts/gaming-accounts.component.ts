a1b26cd3388034f98b4379b189b8afea577206c9
Olzhas Kaliyev <o.kaliev@luxystech.com>
2019-08-12 16:38:42 +0600

    ++

--- src/app/pages/gaming-accounts/gaming-accounts.component.ts
+++ src/app/pages/gaming-accounts/gaming-accounts.component.ts
@@ -1,25 +1,20 @@
-import {AfterViewInit, Component, Input, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {Component, Input, OnInit, ViewChild} from '@angular/core';
 import {Router} from '@angular/router';
-import {MatTableDataSource} from '@angular/material/table';
-import {MatSort} from '@angular/material/sort';
-import {MatPaginator} from '@angular/material/paginator';
-
+import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
 import {ListColumn} from '../../../@fury/shared/list/list-column.model';
 
-import {Observable, of, ReplaySubject} from 'rxjs';
+import {Observable, ReplaySubject} from 'rxjs';
 import {filter} from 'rxjs/operators';
 
 import {GamingAccountsService} from '../../shared/services/gaming-accounts.service';
 import {GamingAccount} from '../../shared/models/gaming-account.model';
-import {GAMING_ACCOUNTS_DEMO_DATA} from '../../../assets/gaming-accounts.demo';
 
 @Component({
   selector: 'fury-gaming-accounts',
   templateUrl: './gaming-accounts.component.html',
   styleUrls: ['./gaming-accounts.component.scss']
 })
-
-export class GamingAccountsComponent implements OnInit, AfterViewInit, OnDestroy {
+export class GamingAccountsComponent implements OnInit {
 
   subject$: ReplaySubject<GamingAccount[]> = new ReplaySubject<GamingAccount[]>(1);
   data$: Observable<GamingAccount[]> = this.subject$.asObservable();
@@ -32,13 +27,13 @@ export class GamingAccountsComponent implements OnInit, AfterViewInit, OnDestroy
     {name: 'Checkbox', property: 'checkbox', visible: true},
     {name: 'Id', property: 'id', visible: true, isModelProperty: true},
     {name: 'Name', property: 'name', visible: true, isModelProperty: true},
-    {name: 'Social link', property: 'social_link', visible: true, isModelProperty: true},
+    {name: 'Social link', property: 'socialLink', visible: true, isModelProperty: true},
     {name: 'Email', property: 'email', visible: true, isModelProperty: true},
-    {name: 'Exp Points', property: 'exp_points', visible: true, isModelProperty: true},
+    {name: 'Exp Points', property: 'expPoints', visible: true, isModelProperty: true},
     {name: 'Silver', property: 'silver', visible: true, isModelProperty: true},
     {name: 'Gold', property: 'gold', visible: true, isModelProperty: true},
-    {name: 'Player Status', property: 'player_status', visible: true, isModelProperty: true},
-    {name: 'Last Visit', property: 'last_visit', visible: true, isModelProperty: true},
+    {name: 'Player Status', property: 'playerStatus', visible: true, isModelProperty: true},
+    {name: 'Last Visit', property: 'lastVisit', visible: true, isModelProperty: true},
     {name: 'Actions', property: 'actions', visible: true},
   ] as ListColumn[];
 
@@ -57,24 +52,18 @@ export class GamingAccountsComponent implements OnInit, AfterViewInit, OnDestroy
   }
 
   ngOnInit() {
-
     this.gamingAccountsService.getGamingAccounts().subscribe((page: any) => {
       this.subject$.next(page.docs.map(gamingAccounts => new GamingAccount(gamingAccounts)));
+      this.dataSource = new MatTableDataSource();
+      this.data$.pipe(
+        filter(Boolean)
+      ).subscribe((gamingAccounts) => {
+        this.gamingAccounts = gamingAccounts;
+        this.dataSource.data = gamingAccounts;
+        this.dataSource.sort = this.sort;
+        this.dataSource.paginator = this.paginator;
+      });
     });
-
-    this.dataSource = new MatTableDataSource();
-
-    this.data$.pipe(
-      filter(Boolean)
-    ).subscribe((gamingAccounts) => {
-      this.gamingAccounts = gamingAccounts;
-      this.dataSource.data = gamingAccounts;
-    });
-  }
-
-  ngAfterViewInit() {
-    this.dataSource.sort = this.sort;
-    this.dataSource.paginator = this.paginator;
   }
 
   onFilterChange(value) {
@@ -86,11 +75,7 @@ export class GamingAccountsComponent implements OnInit, AfterViewInit, OnDestroy
     this.dataSource.filter = value;
   }
 
-  ngOnDestroy() {
-  }
-
   gotoGamingAccount(id) {
-    console.log();
     this.router.navigate(['gaming-accounts/', id]);
   }
 }
--- src/app/shared/models/gaming-account.model.ts
+++ src/app/shared/models/gaming-account.model.ts
@@ -1,23 +1,23 @@
 export class GamingAccount {
   id: string;
   name: string;
-  social_link: string;
+  socialLink: string;
   email: string;
-  exp_points: number;
+  expPoints: number;
   silver: number;
   gold: number;
-  player_status: string;
-  last_visit: string;
+  playerStatus: string;
+  lastVisit: string;
 
   constructor(customer) {
     this.id = customer.id;
     this.name = customer.name;
-    this.social_link = customer.social_link;
+    this.socialLink = customer.socialLink;
     this.email = customer.email;
-    this.exp_points = customer.exp_points;
+    this.expPoints = customer.expPoints;
     this.silver = customer.silver;
     this.gold = customer.gold;
-    this.player_status = customer.player_status;
-    this.last_visit = customer.last_visit;
+    this.playerStatus = customer.playerStatus;
+    this.lastVisit = customer.lastVisit;
   }
 }
