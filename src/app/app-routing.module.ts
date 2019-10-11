452fc85bc28e3e266ec840cacf05a095fa8fee84
Olzhas Kaliyev <o.kaliev@luxystech.com>
2019-10-11 17:00:48 +0600

    ++

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import {NgModule} from '@angular/core';
-import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
 import {LayoutComponent} from './layout/layout.component';
 import {AuthGuard} from './auth.guard';
 
